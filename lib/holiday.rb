f5c76d97ddac01f9e350715f1e87a21b6d2c769c
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 20:22:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -125,11 +125,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash.each do |season, seasonal_hashes|
-    seasonal_hashes.find do |holiday, supply_array|
-      if supply_array.include?("BBQ")
-        
-        puts holiday 
-      end 
+    seasonal_hashes.find do {|holiday, supply_array supply_array.include?("BBQ").first}
+      
      end 
   end 
 end
