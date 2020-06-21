c714976422c1f1df5f0eb582b5f60541f248be97
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 19:16:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -111,7 +111,7 @@ def all_supplies_in_holidays(holiday_hash)
       puts "#{season.capitalize}:"
     seasonal_hashes.each do |holiday, supplies|
    
-      puts "  #{holiday.flatten.capitalize}: #{supplies.join(", ")}"
+      puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
     end 
   end 
 end
