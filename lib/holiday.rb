cc88fdb725f747cc1ef46d58205885c8bdda3aed
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 20:01:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -124,7 +124,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season, holiday|
+  holiday_hash.map do |season, holiday|
     
     if holiday.any?("BBQ") 
      return holiday 
