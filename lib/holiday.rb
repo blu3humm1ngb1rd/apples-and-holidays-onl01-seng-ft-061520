381dc695b37e33ef80620d98ce365d239fd9619c
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 19:40:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -141,7 +141,8 @@ def all_holidays_with_bbq(holiday_hash)
    }
    
    holiday_hash.each do |season, seasonal_hashes|
-     if seasonal_hashes.inlcude? 
+     if seasonal_hashes.has_value?("BBQ")
+       puts seasonal_hashes
    
    
    end 
