389ff066d3b3b15f1f8365371663ea88f46670d4
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-21 18:36:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -107,8 +107,14 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, seasonal_hashes|
 
+  end 
 end
+
+
+
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
